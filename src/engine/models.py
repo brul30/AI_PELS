063b1b2cf1bc5ca0a1f1b90b35948f7cc8c0a860
cu3v4n4 <lastfpvshaman009@gmail.com>
2023-12-02 18:16:21 -0800

    feedback

--- src/engine/models.py
+++ src/engine/models.py
@@ -6,7 +6,8 @@ from django.contrib.postgres.fields import ArrayField
 class Word(models.Model):
 
     word = models.CharField(max_length=100)
-    laymans = ArrayField(models.CharField(max_length=100), blank=True)
+    phonetic = ArrayField(models.CharField(max_length=100, blank=True), default=list, blank=True)
+    laymans = ArrayField(models.CharField(max_length=100, blank=True), default=list, blank=True)
 
     def __str__(self):
         return self.word
--- src/engine/views.py
+++ src/engine/views.py
@@ -1,6 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+import re
+import json
+import requests
+import time
+import hashlib
+import os
+import ast
 from .serializers import WordSerializer
 from .models import Word
 
@@ -9,34 +15,103 @@ import json
 
 from pels.env import config
 
+
 from rest_framework import status
 
-@api_view(['POST'])
-def search(request):
+def get_phonetic(word):
 
-    word_data = request.data.get('search')
+    baseURL = config("SPEECHSUPER_URL",default='none')
+    appKey = config("SPEECH_SUPER_APP_KEY",default='none')
+    secretKey = config("SPEECH_SUPER_SECRET_KEY",default='none')
+    timestamp = str(int(time.time()))
+    coreType = "word.eval"         # Change the coreType according to your needs.
+    refText = f"{word}"        # Change the reference text according to your needs.
+    audioType = "wav"              # Change the audio type corresponding to the audio file.
+    audioSampleRate = 16000
+    userId = "guest"
+    url =  baseURL + coreType
+    connectStr = (appKey + timestamp + secretKey).encode("utf-8")
+    connectSig = hashlib.sha1(connectStr).hexdigest()
+    startStr = (appKey + timestamp + userId + secretKey).encode("utf-8")
+    startSig = hashlib.sha1(startStr).hexdigest()
+    params={
+        "connect":{
+            "cmd":"connect",
+            "param":{
+                "sdk":{
+                    "version":16777472,
+                    "source":9,
+                    "protocol":2
+                },
+                "app":{
+                    "applicationId":appKey,
+                    "sig":connectSig,
+                    "timestamp":timestamp
+                }
+            }
+        },
+        "start":{
+            "cmd":"start",
+            "param":{
+                "app":{
+                    "userId":userId,
+                    "applicationId":appKey,
+                    "timestamp":timestamp,
+                    "sig":startSig
+                },
+                "audio":{
+                    "audioType":audioType,
+                    "channel":1,
+                    "sampleBytes":2,
+                    "sampleRate":audioSampleRate
+                },
+                "request":{
+                    "coreType":coreType,
+                    "refText":refText,
+                    "tokenId":"tokenId"
+                }
+            }
+        }
+    }
+    datas=json.dumps(params)
+    data={'text':datas}
+    headers={"Request-Index":"0"}
+    res=requests.post(url, data=data, headers=headers).text.encode('utf-8', 'ignore').decode('utf-8')
+    data = json.loads(res)
+    phonetics = [stress_entry['phonetic'] for stress_entry in data['result']['words'][0]['scores']['stress']]
+    print(res)
+    return phonetics
 
-    #print(word_data)
-    
-    # Validate that word_data exists and is a string (this is a simple validation example)
-    if not word_data or not isinstance(word_data, str):
-        return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+def phonetic_to_laymans(phonetic, word):
 
-    # Check if the word exists
-    if Word.objects.filter(word=word_data).exists():
+    OPENAI_SECRET_KEY = config("OPENAI_SECRET_KEY",default='none')
+    OPENAI_ENDPOINT = config("OPENAI_ENDPOINT",default='none')
+    prompt = f"Convert {phonetic} from {word} to simple American layman's pronunciation. Give me the array ONLY, seperate each syllable."
+    message =[{"role": "user", "content" : prompt}]
+    model = 'gpt-4-1106-preview'
 
-        serializer = WordSerializer(Word.objects.get(word=word_data))
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_SECRET_KEY}",
+    }
 
-        return Response({'info': "word was found in database",
-                         **serializer.data})
-    
+    data = {
+        "model": model,
+        "messages": message,
+        "temperature": 0,
+    }
+
+    response = requests.post(OPENAI_ENDPOINT, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:      
+        #print(response.json())
+        answer = response.json()["choices"][0]["message"]["content"]
+        print(answer)
+        pattern = r"[\"'](.*?)[\"']"
+        laymans = re.findall(pattern, answer)
+        return laymans
     else:
-        try:
-            laymans = get_laymans(word_data)
-            create_word(word_data, laymans)
-            return search_word(word_data)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        raise Exception(f"Error {response.status_code}: {response.text}")
 
 def search_word(word):
 
@@ -49,88 +124,103 @@ def search_word(word):
 
         serializer = WordSerializer(Word.objects.get(word=word))
 
-        return Response({'info': "word was not found in database, created new word",
+        return Response({'info': "word was found in database, created new word",
                          **serializer.data})
     
     else:
         
         return Response({'error': "word was not found in database"})
-   
+
 def get_laymans(word):
 
-    #print("laymans reached", word)
-
-    message =[{"role": "user", "content" : f"generate english layman pronunciation of {word} in the format: abc-def-ghi. no extra symbols"}]
-
-    key = config("OPENAI_SECRET_KEY", default='none')
-    endpoint = config("OPENAI_ENDPOINT", default='none')
-
-    headers = {
-
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {key}",
-    }
-
-    data = {
-
-        "model": "gpt-4-0613",
-        "messages": message,
-        "temperature": 0,
-    }
-
-    response = requests.post(endpoint, headers=headers, data=json.dumps(data))
-
-    if response.status_code == 200:
-
-        laymans = response.json()['choices'][0]['message']['content']
-        #laymans_list = re.split('-', laymans)
-        laymans_list = laymans.split('-')
-        #print(laymans_list)
-        return laymans_list
-    else:
-        raise Exception(f"Error {response.status_code}: {response.text}")
+    phonetic = get_phonetic(word)
+    laymans = phonetic_to_laymans(phonetic, word)
+    return phonetic, laymans
     
-def create_word(word, laymans):
+def create_word(word, phonetic, laymans):
 
-    #print("create_word reached", word, laymans)
-
-    word = Word(word=word, laymans=laymans)
+    word = Word(word=word, phonetic=phonetic, laymans=laymans)
     word.save()
 
+def get_laymans_from_database(word):
+    
+        if not word or not isinstance(word, str):
+    
+            return None
+    
+        # Check if the word exists
+        if Word.objects.filter(word=word).exists():
+
+            print("word found in database")
+    
+            return Word.objects.get(word=word)
+        
+        else:
+            
+            return None
+
+@api_view(['POST'])
+def search(request):
+
+    word = request.data.get('search')
+    
+    if not word or not isinstance(word, str):
+        return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Check if the word exists
+    if Word.objects.filter(word=word).exists():
+
+        serializer = WordSerializer(Word.objects.get(word=word))
+
+        return Response({'info': "word was found in database",
+                         **serializer.data})
+    
+    else:
+        
+        print("word not found in database")
+        phonetic, laymans = get_laymans(word)
+        create_word(word, phonetic, laymans)
+        return search_word(word)
 
 @api_view(['POST'])
 def feedback(request):
-    mispronounced_phoneme=request.data.get('mispronounciation')
+    
+    scores = request.data.get('response')
     word = request.data.get('word')
-    laymans = request.data.get('laymans')
-    message =[{"role": "user", "content" : f"In one sentece Generate advice for correcting mispronunciation of '{mispronounced_phoneme}' from the word {word} with laysmans {laymans}. Use the following as reference, for the laymans of in-tuh-lek-choo-uhl, the user misspronounced the choo and the feedback is as follow: Try to say choo instead of shoo How to improve Try to block the air with your tongue then relax it slightly to let air out for a 'sh' sound."}]
+    laymans = get_laymans_from_database(word)
+    #print(laymans.laymans)
 
+    response_feedbacks = []
+    response_laymans = []
 
+    for i, x in enumerate(scores):
 
-    key = config("OPENAI_SECRET_KEY", default='none')
-    endpoint = config("OPENAI_ENDPOINT", default='none')
+        if x.get('overall') <= 70:
+            #print(x.get('overall'), laymans.laymans[i])
+            response_laymans.append({"phrase": laymans.laymans[i], "score": x.get('overall')})
+            OPENAI_SECRET_KEY = config("OPENAI_SECRET_KEY",default='none')
+            OPENAI_ENDPOINT = config("OPENAI_ENDPOINT",default='none')
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {OPENAI_SECRET_KEY}",
+            }
 
-    headers = {
+            prompt = f"For '{laymans.laymans[i]}' in '{word}', give a concise articulation tip. One sentence only."
+            message = [{"role": "user", "content": prompt}]
+            model = 'gpt-4-1106-preview'
+            data = {
+                "model": model,
+                "messages": message,
+                "temperature": 1,
+            }
 
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {key}",
-    }
+            response = requests.post(OPENAI_ENDPOINT, headers=headers, data=json.dumps(data))
 
-    data = {
-
-        "model": "gpt-4-0613",
-        "messages": message,
-        "temperature": 0,
-    }
-
-    response = requests.post(endpoint, headers=headers, data=json.dumps(data))
-
-    if response.status_code == 200:
-
-        output = response.json()['choices'][0]['message']['content']
-        #laymans_list = re.split('-', laymans)
-        #laymans_list = laymans.split('-')
-        #print(laymans_list)
-        return Response({"feedback":output})
-    else:
-        return Response(f"Error {response.status_code}: {response.text}")
+            if response.status_code == 200:
+                answer = response.json()["choices"][0]["message"]["content"]
+                response_feedbacks.append({"phrase": laymans.laymans[i], "suggestion": answer})
+            else:
+                raise Exception(f"Error {response.status_code}: {response.text}")
+            
+    return Response(data={"laymans": response_laymans, "feedbacks": response_feedbacks}, status=status.HTTP_200_OK)
+    
